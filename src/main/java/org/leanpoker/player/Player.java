a85013d9e9d8609232b310070a6e0b9d0d56e086
matyasczuczor <matyas.czuczor@gmail.com>
2019-08-26 10:36:14 +0200

    bet request changed to 10

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "1.0";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        return 10;
     }
 
     public static void showdown(JsonElement game) {
