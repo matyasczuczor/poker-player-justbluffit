2632b41b5b25cf258af1aa704c09384c499b30a1
Nees612 <vekker15@gmaill.com>
2019-08-26 14:25:18 +0200

    Pair check implemented

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "1.1";
 
     public static int betRequest(JsonElement request) {
-        return 300;
+        return 1000;
     }
 
     public static void showdown(JsonElement game) {
