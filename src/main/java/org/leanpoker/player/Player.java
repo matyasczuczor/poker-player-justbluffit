f67ab6f8be358d355daa69a537c8c373c08a0fcb
matyasczuczor <matyas.czuczor@gmail.com>
2019-08-26 10:53:54 +0200

    and version 1.0.5 fold 995

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,10 +6,10 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "1.0.4";
+    static final String VERSION = "1.0.5";
 
     public static int betRequest(JsonElement request) {
-        return 1000;
+        return 995;
     }
 
     public static void showdown(JsonElement game) {
