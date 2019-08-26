a6ebadb5ccb4c307b43a2678b8656eca74c81cb6
matyasczuczor <matyas.czuczor@gmail.com>
2019-08-26 16:36:45 +0200

    version 1.4 flop pair check

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,18 +6,21 @@ import com.google.gson.JsonElement;
 
 public class Player {
 
-    static final String VERSION = "1.3";
+    static final String VERSION = "1.4";
 
     public static int betRequest(JsonElement request) {
         String player_id = request.getAsJsonObject().get("in_action").getAsString();
         JsonArray players = request.getAsJsonObject().get("players").getAsJsonArray();
+        JsonArray flop = request.getAsJsonObject().get("community_cards").getAsJsonArray();
         JsonArray cards = new JsonArray();
         JsonArray highCards = new JsonArray();
+
         for (int i = 0; i < players.size(); i++) {
             if (players.get(i).getAsJsonObject().get("id").getAsString().equals(player_id)) {
-                 cards = players.get(i).getAsJsonObject().get("hole_cards").getAsJsonArray();
+                cards = players.get(i).getAsJsonObject().get("hole_cards").getAsJsonArray();
             }
         }
+
         for (int i = 0; i < cards.size(); i++) {
             if (cards.get(i).getAsJsonObject().get("rank").getAsString().equals("10") ||
                     cards.get(i).getAsJsonObject().get("rank").getAsString().equals("J") ||
@@ -27,8 +30,24 @@ public class Player {
                 highCards.add(cards.get(i));
             }
         }
-        if(highCards.size() >= 1 || cards.get(0).equals(cards.get(1))) {
-            return 500;
+
+        int counterFirst = 1;
+        int counterSecond = 1;
+        for (int i = 0; i < flop.size(); i++) {
+            if (cards.get(0).equals(flop.get(i))) {
+                counterFirst++;
+            }
+            if (cards.get(1).equals(flop.get(i))) {
+                counterSecond++;
+            }
+        }
+
+        if (counterFirst > 1 || counterSecond > 1) {
+            return 499;
+        }
+
+        if (highCards.size() >= 1 || cards.get(0).equals(cards.get(1))) {
+            return 498;
         }
         return 0;
     }
