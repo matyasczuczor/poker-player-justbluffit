2ab1f48bb3c63a3a345d99964e6bc062e7437fed
matyasczuczor <matyas.czuczor@gmail.com>
2019-08-26 10:37:12 +0200

    and version 1.0.1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "1.0";
+    static final String VERSION = "1.0.1";
 
     public static int betRequest(JsonElement request) {
         return 10;
