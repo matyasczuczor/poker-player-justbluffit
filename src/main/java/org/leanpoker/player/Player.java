cdcedd882b6574aa1011f4dea8d7c23dfe486150
matyasczuczor <matyas.czuczor@gmail.com>
2019-08-26 11:46:49 +0200

    version 1.0.6 back to 1000

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,10 +6,10 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "1.0.5";
+    static final String VERSION = "1.0.6";
 
     public static int betRequest(JsonElement request) {
-        return 995;
+        return 1000;
     }
 
     public static void showdown(JsonElement game) {
