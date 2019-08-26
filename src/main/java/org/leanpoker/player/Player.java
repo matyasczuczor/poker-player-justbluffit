be5c44b97f38b60bea4c15e6fbebb60479220fd9
Nees612 <vekker15@gmaill.com>
2019-08-26 14:24:36 +0200

    Pair check implemented

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,7 +2,6 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
-
 import java.util.Map;
 
 public class Player {
@@ -10,28 +9,7 @@ public class Player {
     static final String VERSION = "1.1";
 
     public static int betRequest(JsonElement request) {
-        String id = request.getAsString("in_action");
-        JsonArray hand = new JsonArray();
-        JSONArray players = request.getJSONArray("players");
-        String stack;
-        for(int i = 0 ; i < players.length() ; i++){
-            if(players.get(i).get("id").equals(id)){
-                hand = players.get(i).getJSONArray("hole_cards");
-                stack = players.get(i).get("stack");
-            }
-        }
-
-        String[] ranks = new String[2];
-        for(int i = 0 ; i < hand.length() ; i++){
-            ranks[i] = hand.get(i).get("rank");
-        }
-
-        if(ranks[0].equals(ranks[1])){
-            return Integer.parseInt(stack);
-        }else {
-            return 0;
-        }
-
+        return 300;
     }
 
     public static void showdown(JsonElement game) {
